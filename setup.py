0684b25828d9cc64386110cdcb52025f11d89139
Tim Erickson <tim@in2words.org>
2020-12-19 10:26:17 -0800

    Update setup.py with url and classifiers

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'KMyMoney (.kmy) file parser'
+URL = 'https://github.com/timerickson/kmy'
 LONG_DESCRIPTION = 'A simply library to read and provide typed access to KMyMoney data in .kmy files.' \
                    'It currently only supports readonly access.'
 
 # Setting up
 setup(
-    # the name must match the folder name 'kmy'
     name="kmy",
     version=VERSION,
+    url=URL,
     author="Tim Erickson",
     author_email="<tim@timerickson.io>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],  # add any additional packages that
-    # needs to be installed along with your package. Eg: 'caer'
-
+    install_requires=[],
     keywords=['python', 'KMyMoney', 'kmy'],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Intended Audience :: Finance",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
